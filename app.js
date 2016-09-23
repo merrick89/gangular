d056e3cd6f05716309dd2b282e0bb77bd7a0494f
Merrick Chun <merrickc@Blues.local>
2016-09-23 10:59:04 -0400

    Use ng-repeat for an array of products

--- app.js
+++ app.js
@@ -2,13 +2,24 @@
 	var app = angular.module('store', []);
 
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 
-	var gem = {
-		name: 'Dodecahedron',
-		price: 2.95,
-		description: 'asdfasdfasdf',
-	}
+	var gems = [
+		{
+			name: 'Dodecahedron',
+			price: 2,
+			description: 'Some gems have hidden qualities beyond their luster. The Dodecahedron will 100% of the time reflect light into your eyes.',
+			canPurchase: true,
+			soldOut: true
+		},
+		{
+			name: 'Pentagonal Gem',
+			price: 5.95,
+			description: 'This gem has 5 sides. It has 5 sides that can cut you.',
+			canPurchase: true,
+			soldOut: false
+		}
+	];
 
 })();
