0a16aca34de2bf621ef2467464e066aad81aba7a
Merrick Chun <merrickc@Blues.local>
2016-09-26 13:14:40 -0400

    Add form and form validation

--- app.js
+++ app.js
@@ -16,6 +16,15 @@
 		};
 	});
 
+	app.controller('ReviewController', function(){
+		this.review = {};
+
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+	});
+
 	var gems = [
 		{
 			name: 'Dodecahedron',
@@ -23,7 +32,19 @@
 			description: 'Some gems have hidden qualities beyond their luster. The Dodecahedron will 100% of the time reflect light into your eyes.',
 			canPurchase: true,
 			soldOut: true,
-			image: 'images/gem-01.gif'
+			image: 'images/gem-01.gif',
+			reviews:[
+				{
+					stars: 5,
+					body: "I love this product!",
+					author: "joe@thomas.com"
+				},
+				{
+					stars: 3,
+					body: "Product is OK",
+					author: "tim@what.com"
+				}
+			]
 		},
 		{
 			name: 'Pentagonal Gem',
@@ -31,7 +52,19 @@
 			description: 'This gem has 5 sides. It has 5 sides that can cut you.',
 			canPurchase: true,
 			soldOut: false,
-			image: 'images/gem-02.gif'
+			image: 'images/gem-02.gif',
+			reviews:[
+				{
+					stars: 4,
+					body: "I love this product!",
+					author: "joe@thomas.com"
+				},
+				{
+					stars: 1,
+					body: "Product is OK",
+					author: "tim@what.com"
+				}
+			]
 		}
 	];
 
